4604b6066d9f4b81e54d12e434c33d17272bf82c
R.A. Lucas <richard@richardalucas.com>
2014-01-23 18:06:40 -0700

    added promises

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,8 @@
 var MongoClient = require('mongodb').MongoClient
-	, format = require('util').format;
+    , format = require('util').format;
 var _ = require('underscore');
 var ObjectID = require('mongodb').ObjectID;
 var mongoose = require('mongoose');
-var async = require('async');
 var Q = require('q');
 
 var localMongo = 'mongodb://127.0.0.1:27017/rtd';
@@ -30,133 +29,136 @@ var prefixRemove = function(arr) {
     return result;
 };
 
-////
-//api functions
-////
+/*
+* Api Functions
+*
+* TODO: Get away from exports move to better module pattern, perhaps prototype
+*/
 
 //sends entire collection
 exports.findAll = function(req, res) {
-	var collectionName = req.params.collection;
-	MongoClient.connect(MongoUrl, function(err, db) {
-		if(err) throw err;
-		db.collection(collectionName).find({})
-		.limit(10)
-		.toArray(function(err, docs) {
-			res.send(docs);
-		});
-	});
+    var collectionName = req.params.collection;
+    MongoClient.connect(MongoUrl, function(err, db) {
+        if(err) throw err;
+        db.collection(collectionName).find({})
+        .limit(10)
+        .toArray(function(err, docs) {
+            res.send(docs);
+        });
+    });
 };
 
 //finds single document by _id and sends it
 exports.findById = function(req, res, next) {
-	var collectionName = req.params.collection;
-	var id = req.params.id;
-	if(collectionName !== 'javascripts' || collectionName !== 'stylesheets')next();
-		MongoClient.connect(MongoUrl, function(err, db) {
-			if(err) throw err;
-			db.collection(collectionName)
-			.findOne({'_id' : new ObjectID(id)},
-				function(err, doc) {
-					res.send(doc);
-				});
-		});
+    var collectionName = req.params.collection;
+    var id = req.params.id;
+    MongoClient.connect(MongoUrl, function(err, db) {
+        if(err) throw err;
+        db.collection(collectionName)
+        .findOne({ '_id' : new ObjectID(id) },
+            function(err, doc) {
+                res.send(doc);
+            }
+        );
+    });
 };
 
 //not sure this is needed
 exports.findByParameter = function(req, res) {
-	var collectionName = req.params.collection;
-	var parameter = req.params.parameter;
-	MongoClient.connect(MongoUrl, function(err, db) {
-		if(err) throw err;
-		db.collection(collectionName).find({parameter : {}})
-		.limit(10)
-		.toArray(function(err, docs) {
-			res.send(docs);
-		});
-	});
+    var collectionName = req.params.collection;
+    var parameter = req.params.parameter;
+    MongoClient.connect(MongoUrl, function(err, db) {
+        if(err) throw err;
+        db.collection(collectionName).find({parameter : {}})
+        .limit(10)
+        .toArray(function(err, docs) {
+            res.send(docs);
+        });
+    });
 };
 
 //finds a document by it's collection/key/value
 //and sends back those documents that have that
 //value -- need to be able to offer a range here
 exports.findByValue = function(req, res) {
-	var collectionName = req.params.collection;
-	var parameter = req.params.parameter;
-	var value = req.params.value;
-	var pair = {};
-	pair[parameter] = value;
-	MongoClient.connect(MongoUrl, function(err, db) {
-		if(err) throw err;
-		db.collection(collectionName).find(pair)
-		.limit(10)
-		.toArray(function(err, docs) {
-			res.send(docs);
-		});
-	});
+    var collectionName = req.params.collection;
+    var parameter = req.params.parameter;
+    var value = req.params.value;
+    var pair = {};
+    pair[parameter] = value;
+    MongoClient.connect(MongoUrl, function(err, db) {
+        if(err) throw err;
+        db.collection(collectionName).find(pair)
+        .limit(10)
+        .toArray(function(err, docs) {
+            res.send(docs);
+        });
+    });
 };
 
 //creates array of collection names and their associated keys
 exports.getCollectionNames = function(req, res) {
-	MongoClient.connect(MongoUrl, function(err, db) {
-		if(err) throw err;
-		db.collectionNames(function(err, collections) {
-		    console.log(collections);
+    MongoClient.connect(MongoUrl, function(err, db) {
+        if(err) throw err;
+        // TODO: can a promise be put here
+        db.collectionNames(function(err, collections) {
+            console.log(collections);
             var colls = values(collections, 'name');
-			var collNames = prefixRemove(colls);
-			var keyArr = [];
-			collNames.forEach(function(element, index, collNames) {
-				db.collection(element)
-				.findOne({},
-					function(err, docs) {
-					if(docs){
-						var arr = [];
-						for(var key in docs) {
-							arr.push(key);
-						}
-						var keyObj = {};
-						keyObj[element] = arr;
-						keyArr.push(keyObj);
-					}
-                    console.log(keyArr);
-				    //Q(keyArr).done(res.send(keyArr));
-					 setTimeout(function(){
-					 	res.send(keyArr);
-					 },100);
-				});
-			});
-		});
-	});
+            var collNames = prefixRemove(colls);
+            var keyArr = [];
+            // TODO: can a promise be put here
+            collNames.forEach(function(element, index, collNames) {
+                db.collection(element)
+                .findOne({}, function(err, docs) {
+                    if(docs){
+                        var arr = [];
+                        for(var key in docs) {
+                            arr.push(key);
+                        }
+                        var keyObj = {};
+                        keyObj[element] = arr;
+                        keyArr.push(keyObj);
+                    }
+                    Q(keyArr).done(res.send(keyArr));
+                    // setTimeout(function(){
+                    //     res.send(keyArr);
+                    // },100);
+                });
+            });
+        });
+    });
 };
 
 var getCollections = function(arr) {
-	MongoClient.connect(MongoUrl, function(err, db) {
-		if(err) throw err;
-		db.collectionNames(function(err, collections) {
-			arr = collections;
-			console.log('c', arr);
-			return arr;
-		});
-	});
+    MongoClient.connect(MongoUrl, function(err, db) {
+        if(err) throw err;
+        db.collectionNames(function(err, collections) {
+            arr = collections;
+            console.log('c', arr);
+            return arr;
+        });
+    });
 };
 
 exports.getEachCollectionKeys = function(req, res) {
-	MongoClient.connect(MongoUrl, function(err, db) {
-		if(err) throw err;
-		db.collectionNames(function(err, collections) {
-			var colls = values(collections, 'name');
-			var collNames = prefixRemove(colls);
-			var keyArr = [];
-			collNames.forEach(function(element, index, collNames) {
-				db.collection(element)
-				.findOne({}, function(err, docs) {
-					if(docs){
-						keyArr.push(docs);
-					}
-					setTimeout(function() {
-						res.send(keyArr);
-					},100);
-				});
-			});
-		});
-	});
+    MongoClient.connect(MongoUrl, function(err, db) {
+        if(err) throw err;
+        db.collectionNames(function(err, collections) {
+            var colls = values(collections, 'name');
+            var collNames = prefixRemove(colls);
+            var keyArr = [];
+            collNames.forEach(function(element, index, collNames) {
+                db.collection(element)
+                .findOne({}, function(err, docs) {
+                    if(docs){
+                        keyArr.push(docs);
+                    }
+                    Q(keyArr).done(res.send(keyArr));
+                    // setTimeout(function() {
+                    //     res.send(keyArr);
+                    // },100);
+                });
+            });
+        });
+    });
 };
